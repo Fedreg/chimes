71478bb3b6046ac1305e4d23dad7bc8797b9f962
Fed Reggiardo <Fedreg@users.noreply.github.com>
2018-08-30 12:34:17 -0500

    Update state.cljs

--- src/cljs/chimes/state.cljs
+++ src/cljs/chimes/state.cljs
@@ -1,1 +1,16 @@
-fedreggiardo@Feds-MBP.60457
+(ns chimes.state
+  (:require
+   [reagent.core   :as reagent]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; State 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def init-state
+  {:pitch-adjust? true 
+   :wave          :square
+   :voices        1
+   :mode          :lydian})
+
+(defonce state
+  (reagent/atom init-state))
